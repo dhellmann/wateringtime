aef9a0b3ed1d5f9b453ea9a250f3927569138037
Doug Hellmann <doug@doughellmann.com>
2014-05-26 11:28:09 -0400

    clean up Program API a little

--- wateringtime/cal.py
+++ wateringtime/cal.py
@@ -10,43 +10,50 @@ from .program import Program
 
 def show(args, data):
     programs = [Program(*p) for p in data['programs'].items()]
-    programs.sort(key=lambda p: p.starts[0])
-
-    for p in programs:
-        print p.name, p.starts
+    programs.sort(key=lambda p: p.start_times[0])
 
     t = prettytable.PrettyTable(
         field_names=calendar.day_abbr,
         print_empty=False,
         hrules=prettytable.ALL,
     )
-    for d in calendar.day_abbr:
-        t.align[d] = 'l'
+    t.align = 'l'
+
     cal = calendar.Calendar(calendar.MONDAY)
     month_data = cal.monthdayscalendar(args.year, args.month)
+
     for week in month_data:
         row = []
         for n, day in enumerate(week):
             if not day:
+                # Zero days are from another month; leave the cell blank.
                 row.append('')
                 continue
+
+            # Show the day and all watering events on that day.
             lines = ['(%s)' % day]
             for p in (p for p in programs
                       if p.occurs_on_day(n, day)):
                 if args.verbose:
                     lines.append('')
-                    lines.append('%s (%s)' % (p.name, p.days))
-                for s, e, z in p.get_run_times():
+                    lines.append('{name} ({days})'.format(name=p.name,
+                                                          days=p.days))
+                for s, e, z in p.run_times:
                     name = data['zones'][z]
                     lines.append(
-                        '%s-%s - %s' %
-                        (s.strftime('%H:%M'), e.strftime('%H:%M'), name)
+                        '{s}-{e} - {name}'.format(
+                            s=s.strftime('%H:%M'),
+                            e=e.strftime('%H:%M'),
+                            name=name,
+                        )
                     )
             row.append('\n'.join(lines))
         t.add_row(row)
 
     formatted = t.get_string()
-    width = len(formatted.splitlines()[0])
-    format_str = '\n{:^%d}\n' % width
-    print format_str.format(calendar.month_name[args.month])
+    # Center the name of the month over the output calendar.
+    print '\n{:^{width}}\n'.format(
+        calendar.month_name[args.month],
+        width=len(formatted.splitlines()[0]),
+    )
     print formatted
--- wateringtime/program.py
+++ wateringtime/program.py
@@ -22,54 +22,27 @@ class Program(object):
         self.name = name
         self.data = pdata
         self.days = pdata['days']
-        self._day_checker = self._get_day_checker(self.days)
-        self.starts = self._get_start_times(self.data['start'])
+        self._day_checker = self._make_day_checker(self.days)
 
     @property
     def zones(self):
+        """Returns the zones used in the program, sorted by zone id.
+        """
         return sorted(self.data['zones'], key=operator.itemgetter('zone'))
 
     def occurs_on_day(self, dow, dom):
+        """Tests whether the program runs on a given day.
+
+        :param dow: Day of week
+        :param dom: Day of month
+        """
         return self._day_checker(dow, dom)
 
-    def _get_start_times(self, times):
-        return sorted(datetime.datetime.strptime(t, '%H:%M').time()
-                      for t in times)
-
-    @staticmethod
-    def _check_odd(dow, dom):
-        return bool(dom % 2)
-
-    @staticmethod
-    def _check_even(dow, dom):
-        return not bool(dom % 2)
-
-    @staticmethod
-    def _check_dow(dow, dom, valid):
-        return dow in valid
-
-    def _get_day_checker(self, s):
-        """Parse a 'days' string
-
-        A days string either contains 'odd', 'even', or 1-2 letter
-        abbreviations for the days of the week.
-        """
-        if s == 'odd':
-            return self._check_odd
-        elif s == 'even':
-            return self._check_even
-        return functools.partial(
-            self._check_dow,
-            valid=[
-                self._day_abbr[m]
-                for m in re.findall('([MTWF]|Tu|Th|Sa|Su)', s)
-            ],
-        )
-
-    def get_run_times(self):
+    @property
+    def run_times(self):
         """Returns iterable of start, end, and zone name tuples.
         """
-        for s in self.starts:
+        for s in self.start_times:
             for z in self.zones:
                 # FIXME: Convert to datetime and use timedelta?
                 h, m = s.hour, s.minute
@@ -79,3 +52,25 @@ class Program(object):
                 e = datetime.time(h, m)
                 yield s, e, z['zone']
                 s = e
+
+    @property
+    def start_times(self):
+        return sorted(datetime.datetime.strptime(t, '%H:%M').time()
+                      for t in self.data['start'])
+
+    def _make_day_checker(self, s):
+        """Parse a 'days' string
+
+        A days string either contains 'odd', 'even', or 1-2 letter
+        abbreviations for the days of the week.
+        """
+        if s == 'odd':
+            return lambda dow, dom: bool(dom % 2)
+        elif s == 'even':
+            return lambda dow, dom: not bool(dom % 2)
+        else:
+            valid=[
+                self._day_abbr[m]
+                for m in re.findall('([MTWF]|Tu|Th|Sa|Su)', s)
+            ]
+            return lambda dow, dom, valid=valid: dow in valid
