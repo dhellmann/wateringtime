8b6627c2dcfdc12e416a03486619391b1c343feb
Doug Hellmann <doug@doughellmann.com>
2016-09-05 14:11:24 -0400

    show zone names inline in simple view
    Drop the separate table and replace the zone number with the zone name.

--- wateringtime/simple.py
+++ wateringtime/simple.py
@@ -3,21 +3,6 @@ import operator
 import prettytable
 
 
-def show_zones(data):
-    t = prettytable.PrettyTable(
-        field_names=('Zone', 'Name'),
-        print_empty=False,
-    )
-    t.padding_width = 1
-    t.align['Zone'] = 'r'
-    t.align['Name'] = 'l'
-
-    for z in sorted(data['zones'].items()):
-        t.add_row(z)
-
-    print t.get_string()
-
-
 def show_programs(data):
     t = prettytable.PrettyTable(
         field_names=('Program', 'Start Times', 'Days', 'Zones'),
@@ -27,14 +12,18 @@ def show_programs(data):
     t.align['Zones'] = 'l'
 
     for p, pdata in sorted(data['programs'].items()):
-        zones = '\n'.join('%(zone)s(%(time)s)' % z
-                          for z in sorted(pdata['zones'],
-                                          key=operator.itemgetter('zone')))
+        zones = '\n'.join(
+            '{zone:<10}({time})'.format(
+                zone=data['zones'][z['zone']],
+                time=z['time'],
+            )
+            for z in sorted(pdata['zones'],
+                            key=operator.itemgetter('zone'))
+        )
         t.add_row((p, '\n'.join(pdata['start']), pdata['days'],
                    zones))
     print t.get_string()
 
 
 def show(args, data):
-    show_zones(data)
     show_programs(data)
